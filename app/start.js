df00412800b412caf965cb3b8682a59106092c0a
corn4life <corn4life-8ed0fe7f@users.noreply.github.com>
2017-05-30 01:12:57 +0000

    Add a bit more skeleton code to Bot. Add a Plugin class.

--- app/start.js
+++ app/start.js
@@ -1,10 +1,11 @@
 import console from 'console';
 
 import Configuration from '../lib/configuration.js';
+import Bot from '../index.js';
 
 //TODO: don't have this hard-coded
 import discordSecretsConfig from './discord.secrets.js';
 
 let config = new Configuration(discordSecretsConfig);
-
-console.log(config.toJS());
+let bot = new Bot(config);
+bot.start();
